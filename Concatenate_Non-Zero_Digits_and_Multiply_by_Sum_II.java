6271470683b15d0028fc4a8f57c4bde3f744839a
Payal <207457575+payalgit933@users.noreply.github.com>
2026-07-08 17:26:19 +0530

    Fix comment formatting in Concatenate_Non-Zero_Digits_and_Multiply_by_Sum_II.java

--- Concatenate_Non-Zero_Digits_and_Multiply_by_Sum_II.java
+++ Concatenate_Non-Zero_Digits_and_Multiply_by_Sum_II.java
@@ -2,7 +2,7 @@
 // For each queries[i], extract the substring s[li..ri]. Then, perform the following:
 // Form a new integer x by concatenating all the non-zero digits from the substring in their original order. If there are no non-zero digits, x = 0.
 // Let sum be the sum of digits in x. The answer is x * sum.
-// Return an array of integers answer where answer[i] is the answer to the ith query.
+// Return an array of integers answer where answer[i] is the answer to the ith query. 
 // Since the answers may be very large, return them modulo 109 + 7.
 
 import java.util.*;
